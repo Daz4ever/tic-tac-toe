1f2c6f79d663ca810eb26e8e755ac5a3604ba5d0
Dominic Zenon <dominiczenon@gmail.com>
2016-11-01 23:07:07 -0400

    initial

---
+++ tic-tac-toe.js
@@ -0,0 +1,99 @@
+
+  $(document).ready(function() {
+    var turn = false;
+    var xcounter = 0;
+    var ycounter = 0;
+
+        $('.square').click(function() {
+
+          if (turn === false) {
+            if ($(this).text() === '') {
+              $(this).text('X').css('background-color', '#CC2936');
+            }
+            turn = true;
+          }
+          else if (turn === true) {
+            if ($(this).text() === '') {
+              $(this).text('O').css('background-color', '#08415C');
+            }
+
+            turn = false;
+          }
+          var board = [
+            [$('#one').text(),$('#two').text(),$('#three').text()],
+            [$('#four').text(),$('#five').text(),$('#six').text()],
+            [$('#seven').text(),$('#eight').text(),$('#nine').text()]
+          ];
+          var winner = tictactoe(board);
+
+          if (winner) {
+            $('#playerwins').text(winner + ' Wins!');
+            $('#playerwins').css('display', 'block');
+            $('#continue').css('display', 'block');
+            $('.square').prop('disabled', true);
+              if(winner === 'X'){
+                $('#continue').text("Again? X > O").css({"background-color": "#CC2936", "color": "white", 'border': '10px solid #D0CCD0'});
+                console.log("winner");
+                xcounter++;
+              }
+
+              else if (winner === 'O'){
+                $('#continue').text("Again? O > X").css({'background-color': '#08415C', 'color': 'white', 'border': '10px solid #D0CCD0'});
+                ycounter++;
+              }
+
+
+              $('#xscore').text(xcounter.toString());
+
+              $('#yscore').text(ycounter.toString());
+          }
+
+        });
+
+        $('#continue').click(function() {
+            $('.square').prop('disabled', false);
+            $('.square').text('').css('background-color', 'white');
+            $('#playerwins').css('display', 'none');
+            $('#continue').css('display', 'none');
+        });
+
+    });
+
+
+
+
+
+
+//----------------------
+function tictactoe(board) {
+
+   function check(player) {
+     var topLeft = board[0][0] === player,
+         topMid = board[0][1] === player,
+         topRight = board[0][2] === player,
+         midLeft = board[1][0] === player,
+         mid = board[1][1] === player,
+         midRight = board[1][2] === player,
+         btmLeft = board[2][0] === player,
+         btmMid = board[2][1] === player,
+         btmRight = board[2][2] === player;
+
+     if (
+       (topLeft && topMid && topRight) ||
+       (btmLeft && btmMid && btmRight) ||
+       (topLeft && midLeft && btmLeft) ||
+       (topRight && midRight && btmRight) ||
+       (midLeft && mid && midRight) ||
+       (topMid && mid && btmMid) ||
+       (topLeft && mid && btmRight) ||
+       (btmLeft && mid &&topRight)
+     ) {
+       return player;
+     }
+     return null;
+   }
+
+   var x = check('X'),
+       o = check('O');
+   return x || o;
+  }
